4d8519f3baddc59541d7ff23b48622cbc47693c2
aliasalia <alessia.gennari.pd@gmail.com>
2023-05-08 18:25:03 +0200

    added gitignore and started with the real code. now going back and forward in the menu settings

--- test_asm/cruscotto.c
+++ test_asm/cruscotto.c
@@ -1,8 +1,13 @@
+//* done voluntarily verbose thinking about future assembly code
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
-struct settings_utente {
+#define end_supervisor 6
+#define end_user 4
+
+struct settings_utente
+{
     char data[11];
     char ora[6];
     bool blocco_porte;
@@ -10,9 +15,10 @@ struct settings_utente {
     char check_olio[3];
 };
 
-struct settings_supervisor {
-    int freccie_direzione;
-    char* pressione_gomme;
+struct settings_supervisor
+{
+    int frecce_direzione;
+    char *pressione_gomme;
 };
 
 void initialize_utente(struct settings_utente *utente)
@@ -24,18 +30,102 @@ void initialize_utente(struct settings_utente *utente)
     strcpy(utente->check_olio, "ok");
 }
 
+void initialize_supervisor(struct settings_supervisor *supervisor)
+{
+    supervisor->frecce_direzione = 3;
+}
+
+char *user_mods(int index)
+{
+    if (index == 0)
+        return "data";
+    else if (index == 1)
+        return "ora";
+    else if (index == 2)
+        return "blocco porte";
+    else if (index == 3)
+        return "back home";
+    else if (index == 4)
+        return "check olio";
+    else
+        return "";
+}
+
+char *supervisor_mods(int index)
+{
+    if (index == 5)
+        return "frecce direzione";
+    else if (index == 6)
+        return "pressione gomme";
+}
+
 void main(int argc, char *argv[])
 {
-    if (strcmp(argv[1], "2244") == 0)
-        printf("%s", argv[1]);
-    else
-        printf("utente");
+    //* check if is supervisor
+    int supervisor = 0;
+    for (unsigned int i = 0; i < argc; i++)
+        if (strcmp(argv[i], "2244") == 0)
+            supervisor = 1;
 
+    printf("%d", supervisor);
+
+    //* init settings_utente
     struct settings_utente utente;
 
     initialize_utente(&utente);
+    printf("data: %s", utente.data);
 
-    printf("\n%s", utente.data);
+    //* choose stat to change
+    int index = 0;
+    char c;
+    while (true)
+    {
+        scanf("%c", &c);
+        if (c == '\033')
+        {
+            scanf("%c", &c);
+            if (c == '[')
+            {
+                scanf("%c", &c);
+                //* rollback and forward the menu settings
+                if (c == 'A') //* up
+                {
+                    if (index == 0)
+                    {
+                        if (supervisor == 1)
+                            index = end_supervisor;
+                        else
+                            index = end_user;
+                    }
+                    else
+                        index -= 1;
+                }
+                else if (c == 'B') //* down
+                {
+                    if (index == end_user || index == end_supervisor)
+                        index = 0;
+                    else
+                        index++;
+                }
 
+                //* print position in menu settings
+                if (supervisor == 1)
+                {
+                    char *str = user_mods(index);
+                    if (strcmp(str, "") == 0)
+                        printf("%d: %s", index, supervisor_mods(index));
+                    else
+                        printf("%d: %s", index, str);
+                }
+                else
+                    printf("%d: %s", index, user_mods(index));
 
+                //* select a setting
+                if (c == 'C') //* right
+                {
+                    //TODO
+                }
+            }
+        }
+    }
 }
