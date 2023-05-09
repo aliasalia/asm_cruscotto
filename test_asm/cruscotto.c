528cbb9c1180f76f6a17dd2c183b103c5ef0cad2
aliasalia <alessia.gennari.pd@gmail.com>
2023-05-09 18:08:08 +0200

    now get mod status with right arrow but can't change the mod

--- test_asm/cruscotto.c
+++ test_asm/cruscotto.c
@@ -10,14 +10,14 @@ struct settings_utente
 {
     char data[11];
     char ora[6];
-    bool blocco_porte;
-    bool back_home;
+    char blocco_porte[4];
+    char back_home[4];
     char check_olio[3];
 };
 
-struct settings_supervisor
+struct settings_supervisore
 {
-    int frecce_direzione;
+    char *frecce_direzione;
     char *pressione_gomme;
 };
 
@@ -25,14 +25,15 @@ void initialize_utente(struct settings_utente *utente)
 {
     strcpy(utente->data, "15/06/2014");
     strcpy(utente->ora, "15:32");
-    utente->blocco_porte = true;
-    utente->back_home = true;
-    strcpy(utente->check_olio, "ok");
+    strcpy(utente->blocco_porte, "ON");
+    strcpy(utente->back_home, "ON");
+    strcpy(utente->check_olio, "OK");
 }
 
-void initialize_supervisor(struct settings_supervisor *supervisor)
+void initialize_supervisor(struct settings_supervisore *supervisore)
 {
-    supervisor->frecce_direzione = 3;
+    strcpy(supervisore->frecce_direzione, "3");
+    strcpy(supervisore->pressione_gomme, "Reset pressione gomme");
 }
 
 char *user_mods(int index)
@@ -59,6 +60,54 @@ char *supervisor_mods(int index)
         return "pressione gomme";
 }
 
+char *user_mods_get(struct settings_utente *utente, int index)
+{
+    if (index == 0)
+        return utente->data;
+    else if (index == 1)
+        return utente->ora;
+    else if (index == 2)
+        return utente->blocco_porte;
+    else if (index == 3)
+        return utente->back_home;
+    else if (index == 4)
+        return utente->check_olio;
+    else
+        return "";
+}
+
+char *supervisor_mods_get(struct settings_supervisore *supervisore, int index)
+{
+    if (index == 5)
+        return supervisore->frecce_direzione;
+    else if (index == 6)
+        return supervisore->pressione_gomme;
+}
+
+char *change_on_off(char *on_off)
+{
+    if (strcmp(on_off, "ON") == 0)
+        return "OFF";
+    else
+        return "ON";
+}
+
+char *change_frecce_direzione(int intensity)
+{
+    if (intensity > 5)
+        return "5";
+    else if (intensity < 2)
+        return "2";
+    else
+    {
+        if (intensity % 3 == 0)
+            return "3";
+        else if (intensity % 4 == 0)
+            return "4";
+    }
+    return "";
+}
+
 void main(int argc, char *argv[])
 {
     //* check if is supervisor
@@ -74,6 +123,11 @@ void main(int argc, char *argv[])
 
     initialize_utente(&utente);
     printf("data: %s", utente.data);
+
+    struct settings_supervisore supervisore;
+
+    if (supervisor == 1)
+        initialize_supervisor(&supervisore);
 
     //* choose stat to change
     int index = 0;
@@ -123,7 +177,40 @@ void main(int argc, char *argv[])
                 //* select a setting
                 if (c == 'C') //* right
                 {
-                    //TODO
+                    //* see the setting specs
+                    if (supervisor == 1)
+                    {
+                        char *str = user_mods_get(&utente, index);
+                        if (strcmp(str, "") == 0)
+                            printf(": %s", supervisor_mods_get(&supervisore, index));
+                        else
+                            printf(": %s", str);
+                    }
+                    else
+                        printf(": %s", user_mods_get(&utente, index));
+
+                    scanf("%c", &c);
+                    //TODO not working
+                    //* change the changable settings for ON/OFF
+                    if ((c == 'A' || c == 'B') && index == 2)
+                    {
+                        strcpy(utente.blocco_porte, change_on_off(utente.blocco_porte));
+                        printf("%s: %s", user_mods(index), utente.blocco_porte);
+                    }
+                    else if ((c == 'A' || c == 'B') && index == 3)
+                    {
+                        strcpy(utente.back_home, change_on_off(utente.back_home));
+                        printf("%s: %s", user_mods(index), utente.back_home);
+                    }
+                    //* change the changable settings for frecce
+                    if (index == 5)
+                    {
+                        strcpy(supervisore.frecce_direzione, change_frecce_direzione(c));
+                        printf("%s: %s", user_mods(index), supervisore.frecce_direzione);
+                    }
+                    //* change the changable settings for pressione gomme
+                    if (index == 6 && c == 'C')
+                        printf("%s", supervisore.pressione_gomme);
                 }
             }
         }
