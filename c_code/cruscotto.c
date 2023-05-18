ed1096f10ef8c18b7c2065131422d811a0f75b54
PacMat99 <pacchinmattia@gmail.com>
2023-05-18 22:45:01 +0200

    c code done

--- c_code/cruscotto.c
+++ c_code/cruscotto.c
@@ -6,14 +6,16 @@ int ind = 1;
 int sub = 0;
 int door_lock = 1;
 int back_home = 1;
+int blinkers = 3;
 
 int move() {
-    char c;
+    char c, tmp;
     scanf("%c", &c);
     if (c == '\033') {
         scanf("%c", &c);
         if (c == '[') {
             scanf("%c", &c);
+            scanf("%c", &tmp);
             if (c == 'A') //* up
                 return -1;
             else if (c == 'B') //* down
@@ -25,19 +27,32 @@ int move() {
     return 0;
 }
 
+void set_blinkers() {
+    int n;
+    char tmp;
+    scanf("%d", &n);
+    scanf("%c", &tmp);
+    if (n > 5)
+        blinkers = 5;
+    else if (n < 2)
+        blinkers = 2;
+    else
+        blinkers = n;
+}
+
 void index_position_message(int supervisor) {
     switch (ind) {
         case 1:
             if (supervisor == 1)
-                printf("1. Setting automobile (supervisor):");
+                printf("1. Setting automobile (supervisor): ");
             else
-                printf("1. Setting automobile:");
+                printf("1. Setting automobile: ");
             break;
         case 2:
-            printf("2. Data: 15/06/2014");
+            printf("2. Data: 15/06/2014 ");
             break;
         case 3:
-            printf("3. Ora: 15:32");
+            printf("3. Ora: 15:32 ");
             break;
         case 4:
             if (sub) {
@@ -46,12 +61,10 @@ void index_position_message(int supervisor) {
                     door_lock = !door_lock;
                 sub = 0;
             }
-            else {
-                if (back_home)
-                    printf("4. Blocco automatico porte: ON");
-                else
-                    printf("4. Blocco automatico porte: OFF");
-            }
+            if (door_lock)
+                printf("4. Blocco automatico porte: ON ");
+            else
+                printf("4. Blocco automatico porte: OFF ");
             break;
         case 5:
             if (sub) {
@@ -60,39 +73,34 @@ void index_position_message(int supervisor) {
                     back_home = !back_home;
                 sub = 0;
             }
-            else {
-                if (back_home)
-                    printf("5. Back-home: ON");
-                else
-                    printf("5. Back-home: OFF");
-            }
+            if (back_home)
+                printf("5. Back-home: ON ");
+            else
+                printf("5. Back-home: OFF ");
             break;
         case 6:
-            printf("6. Check olio");
+            printf("6. Check olio ");
             break;
         case 7:
             if (sub) {
-                //scanf(...);
+                set_blinkers();
                 sub = 0;
             }
-            else {
-                printf("7. Frecce direzione");
-            }
+            printf("7. Frecce direzione: %d ", blinkers);
             break;
         case 8:
-            printf("8. Reset pressione gomme");
             if (sub) {
-                printf("Pressione gomme resettata");
+                printf("Pressione gomme resettata\n");
                 sub = 0;
             }
+            printf("8. Reset pressione gomme ");
             break;
     }
 }
 
 void navigate_menu(int supervisor) {
     int read = move();
-    printf("Read: %d\n", read);
-    if (read == 2 && ind != 2 && ind != 3 && ind != 6)
+    if (read == 2 && ind != 1 && ind != 2 && ind != 3 && ind != 6)
         sub = 1;
     else if (read != 2) {
         ind += read;
@@ -116,10 +124,8 @@ int main(int argc, char *argv[]) {
 
     index_position_message(supervisor);
 
-    char tmp;
     while (1) {
         navigate_menu(supervisor);
-        scanf("%c", &tmp);
     }
 
     return 0;
