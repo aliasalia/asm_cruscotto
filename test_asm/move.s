ecc220f70db1f1bfa67cd04016396c69484b1211
aliasalia <alessia.gennari.pd@gmail.com>
2023-06-12 19:48:29 +0200

    mod

--- test_asm/move.s
+++ test_asm/move.s
@@ -5,62 +5,55 @@
 .section .data
     c:              .ascii "0"
     c_length:       .long . - c
-    tmp:            .ascii "0"
-    tmp_length:     .long . - tmp
+    up:             .ascii "A"
+    up_length:      .long . -up
+    down:           .ascii "B"
+    down_length:    .long . -down
+    right:          .ascii "C"
+    right_length:   .long . -right
 
 .section .text
-    .global _move
+    .global _start
 
     #.type move, @function       # function declaration
 
-_move:
+_start:
     movl $3, %eax           # scanf first char
     movl $0, %ebx
     leal c, %ecx
     movl c_length, %edx
     int $0x80               # invoke the Linux kernel to perform system call
 
-    cmpl $27, %ecx           # cmp '\033' to %eax
-    jne .end
+    movl up, %eax
+    cmpl %eax, %ecx
+    je is_up
+    xor %eax, %eax
+    movl down, %eax
+    cmpl %eax, %ecx
+    je is_down
+    xor %eax, %eax
+    movl right, %eax
+    cmpl %eax, %ecx
+    je right
 
-    movl $3, %eax           # scanf second char
-    movl $0, %ebx
-    leal c, %ecx
-    movl c_length, %edx
-    int $0x80               # invoke the Linux kernel to perform system call
-
-    cmpl $'[', %ecx          # cmp '[' to %eax
-    jne .end
-
-    movl $3, %eax           # scanf third char (A B or C)
-    movl $0, %ebx
-    leal c, %ecx
-    movl c_length, %edx
-    int $0x80               # Invoke the Linux kernel to perform system call
-
-    # check wich key has been pressed
-    cmpl $'A', %ecx
-    je .up
-    cmpl $'B', %ecx
-    je .down
-    cmpl $'C', %ecx
-    je .right
-
-.up:
+is_up:
     # return -1
     movl $-1, %eax
-    jmp .end
+    jmp end
 
-.down:
+is_down:
     # return 1
     movl $1, %eax
-    jmp .end
+    jmp end
 
-.right:
+is_right:
     # return 2
     movl $2, %eax
-    jmp .end
+    jmp end
 
-.end:
+end:
     # end of function
-    ret
+    #ret
+    movl $1, %eax			
+	movl $0, %ebx			
+	int $0x80	
